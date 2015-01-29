e3c2aa077e4de3e5d2d99e3d6b749e747afabc89
Michael Bartling <michael.bartling15@gmail.com>
2015-01-28 18:42:25 -0600

    sdds

---
+++ main.d
@@ -0,0 +1,4 @@
+.\main.o: main.c
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\main.o: PLL.h
+.\main.o: UART0.h
---
+++ uart0.d
@@ -0,0 +1,8 @@
+.\uart0.o: UART0.c
+.\uart0.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\uart0.o: ..\..\LaunchPadware\inc/tm4c123gh6pm.h
+.\uart0.o: FIFO.h
+.\uart0.o: UART0.h
+.\uart0.o: UART0_debug.h
+.\uart0.o: debug_settings.h
+.\uart0.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
